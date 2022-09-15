7a3e004e152d76d5b8be32573e456bca26e186cd
Marcelle Pernet <mevpernet@gmail.com>
2022-09-14 22:41:21 -0300

    by ID ok

--- public/home.js
+++ public/home.js
@@ -12,8 +12,26 @@ refreshSubject.addEventListener('click', async function newBySubject() {
     let data = await fetch('https://isaboscov-api.herokuapp.com/quotes/Angelina Jolie');
     let result = await data.json();
     let index = Math.floor(Math.random() * (result.length));
-    console.log(index);
 
     let quote = document.querySelector('.bysubject .quote');
     quote.innerText = result[index].quote;
 })
+
+document.querySelector('.byID .refresh').onclick = async () => {
+    let quoteslist = await fetch('https://isaboscov-api.herokuapp.com/quotes');
+    let json = await quoteslist.json();
+    let randomID = Math.ceil(Math.random() * (json.length - 1));
+    console.log(randomID);
+
+    let link = document.querySelector('.byID .endpoint .idlink');
+    link.setAttribute('href', `https://isaboscov-api.herokuapp.com/quotes/id/${randomID}`);
+
+    let textLink = document.querySelector('.byID .endpoint .id');
+    textLink.innerText = randomID;
+
+    let data = await fetch(`https://isaboscov-api.herokuapp.com/quotes/id/${randomID}`);
+    let result = await data.json();
+
+    let quote = document.querySelector('.byID .quote');
+    quote.innerText = result.quote;
+}
